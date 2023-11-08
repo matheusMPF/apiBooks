23f60f6e992e1a69d0f7185c0ea031362ecc03fa
Matheus <matheu.mpf18@gmail.com>
2023-11-08 16:29:32 -0300

    Commit 2.0 > Update

--- src/main/java/com/bookApi/BookApiApplication.java
+++ src/main/java/com/bookApi/BookApiApplication.java
@@ -9,5 +9,6 @@ public class BookApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BookApiApplication.class, args);
 	}
-
+	
 }
+
--- src/main/java/com/bookApi/entity/Book.java
+++ src/main/java/com/bookApi/entity/Book.java
@@ -1,8 +1,6 @@
 package com.bookApi.entity;
 
 import java.util.List;
-
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
@@ -14,14 +12,8 @@ public class Book {
 	@Id
 	protected String isbn;
 	protected String title;
-	protected int pages;
 	protected float price;
-	
-	@Column(length = 2500)
-	protected String summary;
 	protected String edition;
-	protected String type;
-	
 	protected boolean highlights;
 	protected boolean active;
 	@ManyToOne
@@ -38,16 +30,13 @@ public class Book {
 	}
 	
 	//composite constructor
-	public Book(String isbn, String title, int pages, float price, String summary, String edition, String type,
+	public Book(String isbn, String title, int pages, float price, String edition,
 			boolean highlights, boolean active) {
 		super();
 		this.isbn = isbn;
 		this.title = title;
-		this.pages = pages;
 		this.price = price;
-		this.summary = summary;
 		this.edition = edition;
-		this.type = type;
 		this.highlights = highlights;
 		this.active = active;
 	}
@@ -68,14 +57,6 @@ public class Book {
 		this.title = title;
 	}
 
-	public int getPages() {
-		return pages;
-	}
-
-	public void setPages(int pages) {
-		this.pages = pages;
-	}
-
 	public float getPrice() {
 		return price;
 	}
@@ -84,28 +65,12 @@ public class Book {
 		this.price = price;
 	}
 
-	public String getSummary() {
-		return summary;
-	}
-
-	public void setSummary(String summary) {
-		this.summary = summary;
-	}
-
 	public String getEdition() {
 		return edition;
 	}
 
 	public void setEdition(String edition) {
 		this.edition = edition;
-	}
-
-	public String getType() {
-		return type;
-	}
-
-	public void setType(String type) {
-		this.type = type;
 	}
 
 	public boolean isHighlights() {
